b96e86ce11332ecaa0d999ddcb2cc047c02e1331
Junnan Tang <junnan.tang.2014@gmail.com>
2016-05-05 17:15:16 -0700

    c
    c

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function start(){
 	document.getElementById("startPage").style.display = "none";
+	document.getElementById("screen").style.display = "block";
 }
 function ins(){
 	document.getElementById("instruction").style.display = "block";
