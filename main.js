b70934d834b79cae3ecb24af18a1f24c4b14eee0
HughSong <forever.love.hugh@gmail.com>
2016-05-10 16:12:48 -0700

    heihei

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 
 function start(){
 	document.getElementById("startPage").style.display = "none";
-	document.getElementById("grid-container").style.display = "block";
+	document.getElementById("sequence1").style.display = "block";
+
+	setTimeout(function ()
+	{
+		document.getElementById("sequence1").style.display = "none";
+		document.getElementById("grid-container").style.display = "block";
+
+	},5000);
 }
 function ins(){
 	document.getElementById("instruction").style.display = "block";
@@ -10,5 +17,13 @@ function HomePage() {
     document.getElementById("instruction").style.display = "none";
 
 }
+
+//设置canvas 并插图
+var S0=document.getElementById("position0");
+var ctx0=S0.getContext("2d");
+var img=new Image();
+img.src="...";
+
+ctx0.drawImage(img,10,10);
 
 
