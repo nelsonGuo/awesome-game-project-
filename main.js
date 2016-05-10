e7ebe54a166bfa2a2c1d59a3da3c20d950528628
Junnan Tang <junnan.tang.2014@gmail.com>
2016-05-10 15:18:45 -0700

    grid

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+
 function start(){
 	document.getElementById("startPage").style.display = "none";
-	document.getElementById("screen").style.display = "block";
+	document.getElementById("grid-container").style.display = "block";
+
 }
+
 function ins(){
 	document.getElementById("instruction").style.display = "block";
 }
+
 function HomePage() {
     document.getElementById("instruction").style.display = "none";
+
 }
+
+
