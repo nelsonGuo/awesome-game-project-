f79c84567f2b2d59602c2903a6cb08dc84c84450
liang611zh <zhaoliangbz@gmail.com>
2016-05-10 15:53:22 -0700

    canvassequence

--- main.js
+++ main.js
@@ -2,13 +2,10 @@
 function start(){
 	document.getElementById("startPage").style.display = "none";
 	document.getElementById("grid-container").style.display = "block";
-
 }
-
 function ins(){
 	document.getElementById("instruction").style.display = "block";
 }
-
 function HomePage() {
     document.getElementById("instruction").style.display = "none";
 
