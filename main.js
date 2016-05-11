d43c0185cd9cbc7c4262b961e930654b81a4ec73
HughSong <forever.love.hugh@gmail.com>
2016-05-10 17:53:27 -0700

    heihei

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ var S0, ctx0, img0;
 var S1, ctx1, img1;
 var S2, ctx2, img2;
 var S3, ctx3, img3;
-var S4, ctx4, img4;
+
+
 
 var map = new Array();
+var sequence = new Array();
+var numClick = 0;
 
 function start() {
 	document.getElementById("startPage").style.display = "none";
@@ -15,33 +18,50 @@ function start() {
 		document.getElementById("sequence1").style.display = "none";
 		document.getElementById("grid-container").style.display = "block";
 
-	}, 5000);
+	}, 3000);
 
+	for(var i = 0; i<4;i++) {
+		addImage(i);
 
-	S0 = document.getElementById("position0");
-	ctx0 = S0.getContext("2d");
-	img0 = document.getElementById("symbol1");
-	ctx0.drawImage(img0,5,5);
+	}
 
-	S1 = document.getElementById("position1");
-	ctx1 = S1.getContext("2d");
-	img1 = document.getElementById("symbol2");
-	ctx1.drawImage(img1, 5, 5);
-
-	S2 = document.getElementById("position2");
-	ctx2 = S2.getContext("2d");
-	img2 = document.getElementById("symbol0");
-	ctx2.drawImage(img2, 5, 5);
-
-	S3 = document.getElementById("position3");
-	ctx3 = S3.getContext("2d");
-	img3 = document.getElementById("symbol1");
-	ctx3.drawImage(img3, 5, 5);
+	for(var i = 0; i<3;i++) {
+		for(var j = 0; j<3;j++){
+			addImageToMap(i,j);
+		}
+	}
 
 
 
 
 }
+
+function addImage(i) {
+	var S = document.getElementById("position" + i);
+	var ctx = S.getContext("2d");
+	var img = document.getElementById("symbol" + i);
+	ctx.drawImage(img,5,5);
+	sequence[i] = i;
+
+}
+
+function addImageToMap(i,j) {
+	var S = document.getElementById("grid-"+i+"-"+j);
+	var ctx = S.getContext("2d");
+	var img = document.getElementById("symbol" + i);
+	map["grid-"+i+"-"+j] = i;
+	ctx.drawImage(img,5,5);
+
+}
+
+function stepMatching(id) {
+	if(sequence[numClick] == map[id])
+		alert("Correct");
+	else
+		alert("Wrong");
+	numClick++;
+}
+
 
 
 function ins(){
