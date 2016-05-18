b8938a5f98dde7a26fb7d0230e1e0e315c4b634a
liang611zh <zhaoliangbz@gmail.com>
2016-05-18 13:44:07 -0700

    pop-up window modify the timer

--- JavaScript/Lose.js
+++ JavaScript/Lose.js
@@ -2,7 +2,7 @@
  * Created by Victor Zhao on 2016/5/17 0017.
  */
 function lose () {
-        if ( life==0) {
+        if ( life==-1 ||timeGame==-1) {
                 openPopup();
         }
 }
--- JavaScript/Timer.js
+++ JavaScript/Timer.js
@@ -1,19 +1,15 @@
-/**
- * Created by 宋与珩 on 2016-05-17.
- */
+
 function gameTimer() {
-    var i = 30;
     var timer = setInterval(function(){
-        if(i== -1){
+        if(timeGame== -1){
             clearInterval(timer);
-            lose();
         }else{
-            document.getElementById("timer").innerHTML = "Time: "+i;
-            --i;
+            document.getElementById("timer").innerHTML = "Time: "+timeGame;
+            --timeGame;
+            lose();
         }
     },1000);
 }
-
 //sequence timer
 function sequenceTimer(){
     var i = 10;
