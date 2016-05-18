7798b747ce292965a118dff8968213825c7afa1c
nelsonGuo <nelsonkwok15@gmail.com>
2016-05-17 23:47:34 -0700

    leaderboard

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    define('DB_HOST', 'mysql12.000webhost.com');
+    define('DB_USER', 'a1138299_liang');
+    define('DB_PASSWORD', 'jdfzzl1990611');
+    define('DB_DATABASE', 'a1138299_liang');
+    define('HOMEURL', 'http://escape-game.comlu.com');
+?>
