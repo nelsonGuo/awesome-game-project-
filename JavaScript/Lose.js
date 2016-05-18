9261489989a134f69e4f2b8feb4cb77b4d8df505
liang611zh <zhaoliangbz@gmail.com>
2016-05-18 14:04:53 -0700

    pop-up window modify the timer

--- JavaScript/Lose.js
+++ JavaScript/Lose.js
@@ -1,6 +1,7 @@
 /**
  * Created by Victor Zhao on 2016/5/17 0017.
  */
+//life or timer to 0 it will popup a input board for the leaderboard.
 function lose () {
         if ( life==-1 ||timeGame==-1) {
                 openPopup();
