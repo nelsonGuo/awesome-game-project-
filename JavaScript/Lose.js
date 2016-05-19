7e78bb624305de1927eb02dd52e0196ef90f6641
liang611zh <zhaoliangbz@gmail.com>
2016-05-18 21:50:52 -0700

    time part

--- JavaScript/Lose.js
+++ JavaScript/Lose.js
@@ -3,7 +3,7 @@
  */
 //life or timer to 0 it will popup a input board for the leaderboard.
 function lose () {
-        if ( life==0 ||timeGame==-1) {
+        if ( life==0 ||timeGame<=0) {
                 openPopup();
         }
         
