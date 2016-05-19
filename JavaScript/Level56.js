11f446db0b9f101c12d689a7680ea9d4770f3c97
HughSong <forever.love.hugh@gmail.com>
2016-05-19 13:31:49 -0700

    I separate my js, and I have made the 4*4 map

--- JavaScript/Level56.js
+++ JavaScript/Level56.js
@@ -1,17 +1,19 @@
 /**
+ * Created by 宋与珩 on 2016-05-19.
+ */
+/**
  * Created by 宋与珩 on 2016-05-17.
  */
-function Level34(){
-    
+function Level56(){
     lvl++;
     timeGame=30;
     map = voidSymbol;
     sequence = voidSymbol;
-    
+
     numClick = 0;
-    
+
     document.getElementById("grid-container").style.display = "none";
- 
+
     setTimeout(function () {
         document.getElementById("level1").style.display = "block";
         document.getElementById("sequence3").style.display = "block";
@@ -19,8 +21,8 @@ function Level34(){
         document.getElementById("top-bar").style.display = "block";
     }, 1000);
     sequenceTimer();
-    
-    
+
+
     setTimeout(function () {
         document.getElementById("sequence3").style.display = "none";
         document.getElementById("level1").style.display = "none";
@@ -32,6 +34,6 @@ function Level34(){
     }, 11000);
     generateSymbol3();
     checkSymbol3();
-    
-    
+
+
 }
