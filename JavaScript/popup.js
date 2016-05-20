edaa30ad7d713b428f2eeb0487716ca32e1a592d
liang611zh <zhaoliangbz@gmail.com>
2016-05-19 23:37:05 -0700

    changes

--- JavaScript/popup.js
+++ JavaScript/popup.js
@@ -15,8 +15,8 @@ function updatePopup(){
 	var $popupContent = $("#popup-content");
 	// http://api.jquery.com/height/
 	// http://api.jquery.com/outerheight/
-	//var top = $(window).height() / 2 - $popupContent.outerHeight() / 2; // Center vertical
-	var top = "300px"; // Fixed offset
+	var top = $(window).height() / 2 - $popupContent.outerHeight() / 2; // Center vertical
+	//var top = "200px"; // Fixed offset
 	// http://api.jquery.com/width/
 	//  http://api.jquery.com/outerWidth/
 	var left = ($(window).width() - $popupContent.outerWidth()) / 2; // Center horizontal
--- JavaScript/rotate.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Victor Zhao on 2016/5/19 0019.
- */
-function rotate() {
-    documen.getElementById("Position1").style.webkitTransform="rotateY(180deg)";
-}
